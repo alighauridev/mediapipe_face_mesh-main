4fa385a0a36a1343c9cfd1286f8245366b5a497a
muhammadalighouri <98492348+muhammadalighouri@users.noreply.github.com>
2023-05-26 17:33:35 +0500

    update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -19,50 +19,7 @@ export default function App() {
                 </div>
 
                 <div className="search-cart-parent">
-                    <div className="search-parent">
-                        <input type="checkbox" name="" id="chkbox" />
-                        <div className="search-bar">
-                            <div className="search-box">
-                                <label htmlFor="chkbox">
-                                    <img
-                                        src={
-                                            "https://lip-gloss-site.netlify.app/static/media/search.ef43d977b5864f47b1d25dc40ba6be29.svg"
-                                        }
-                                        alt=""
-                                    />
-                                </label>
-                            </div>
-                            <input
-                                type="search"
-                                placeholder="Search Here"
-                                className="search-input"
-                            />
-                        </div>
-                    </div>
-                    <div>
-                        <Link to="https://lip-gloss-site.netlify.app/cart" style={{ color: "black" }}>
-                            <span>
-                                <img
-                                    style={{ filter: "invert(1)" }}
-                                    src={
-                                        "https://lip-gloss-site.netlify.app/static/media/cart.966254d1f9ffc712568d21ec48e465f5.svg"
-                                    }
-                                    alt=""
-                                />
-                            </span>
-                            <span
-                                style={{
-                                    fontSize: "10px",
-                                    position: "relative",
-                                    top: "-10px",
-                                    left: "-4px",
-                                }}
-                            >
-                                {" "}
-                                ({0})
-                            </span>
-                        </Link>
-                    </div>
+
                 </div>
             </div>
         </div>
